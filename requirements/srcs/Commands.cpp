1abb609d33edd58840edab3d555af97f85c6777b
abdellah <bellakrim2032@gmail.com>
2023-06-03 23:13:39 +0100

    add nick and quit commands need more test for sanitize

---
+++ requirements/srcs/Commands.cpp
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Commands.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abellakr <abellakr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/03 22:24:11 by abellakr          #+#    #+#             */
+/*   Updated: 2023/06/03 23:12:17 by abellakr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ircserv.head.hpp"
+
+void Server::nick(size_t pfdsindex)
+{
+    if(MS.size() != 2)
+    {
+        ERR_NEEDMOREPARAMS(pfdsindex, MS[0]);
+        return;   
+    }
+    else
+    {
+        if((MS[1][0] >='a' && MS[1][0] <= 'z') || (MS[1][0] >= 'A' && MS[1][0] <= 'Z'))
+        {
+
+            std::map<int,Client>::iterator tmp = ClientsMap.find(pfds[pfdsindex].fd);
+            Client& client = tmp->second;
+            //------
+            std::map<int, Client>::iterator it = ClientsMap.begin();
+            while(it != ClientsMap.end())
+            {
+                if(it->second.getNICKNAME() == MS[1])
+                    break;
+                it++;   
+            }
+            if(it != ClientsMap.end())
+            {
+                ERR_NICKNAMEINUSE(pfdsindex, MS[1]);
+                return;
+            }
+            else if(it == ClientsMap.end())
+                client.setNICKNAME(MS[1]);
+        }
+        else
+        {
+            ERR_ERRONEUSNICKNAME(pfdsindex, MS[1]);
+        }  
+    }
+}
+
+
+bool Server::quit(size_t pfdsindex)
+{
+    if(MS.size() != 1)
+    {
+        ERR_NEEDMOREPARAMS(pfdsindex, MS[0]);
+        return false; 
+    }
+    else
+    {
+        // remove client from map
+        std::cout << "client disconnected sockfd: " << pfds[pfdsindex].fd << std::endl; 
+        std::map<int,Client>::iterator itmap = ClientsMap.find(pfds[pfdsindex].fd);
+        ClientsMap.erase(itmap);
+        // remove client from pollfd vector
+        std::vector<pollfd>::iterator itvec = pfds.begin();
+        itvec += pfdsindex;
+        close(pfds[pfdsindex].fd);
+        pfds.erase(itvec);
+        // remove client from channel
+        return true;
+    }
+    return false;
+}
--- requirements/srcs/Server.class.cpp
+++ requirements/srcs/Server.class.cpp
@@ -6,14 +6,13 @@
 /*   By: abellakr <abellakr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 18:31:53 by abellakr          #+#    #+#             */
-/*   Updated: 2023/05/25 16:39:06 by abellakr         ###   ########.fr       */
+/*   Updated: 2023/06/03 23:09:18 by abellakr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 
 #include "../ircserv.head.hpp"
-// static int counter = 0;
 Server::Server(int PORT, std::string PASSWORD) : PORT(PORT) , PASSWORD(PASSWORD)
 {
     SetupServer();
@@ -297,11 +296,11 @@ void Server::executecommand(size_t pfdsindex)
     if(MS[0] == "BOT" || MS[0] == "bot") // bot
         bot(pfdsindex);
     else if(MS[0] == "NICK" || MS[0] == "nick") // nick 
-        std::cout << "NICK\n";
+        nick(pfdsindex);
     else if(MS[0] == "MODE" || MS[0] == "mode") // mode
         std::cout << "MODE\n";
-    else if(MS[0] == "QUIT" || MS[0] == "quit") // quit
-        std::cout << "QUIT\n";
+    else if((MS[0] == "QUIT" || MS[0] == "quit") && (quit(pfdsindex) == true)) // quit
+        return ;
     else if(MS[0] == "JOIN" || MS[0] == "join") // join
         this->AddChannel(MS[1]);
     else if(MS[0] == "PART" || MS[0] == "part") // part
